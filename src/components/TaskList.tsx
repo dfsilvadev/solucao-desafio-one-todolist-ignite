ac10998fcbeb5109c490d2d11a5961bda8d4d5d0
Daniel Silva <dani3l.dxp@gmail.com>
2021-03-22 08:28:40 -0300

    feat: corrigindo template do projeto

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -60,7 +60,7 @@ export function TaskList() {
         <div className="input-group">
           <input
             type="text"
-            placeholder="Adicionar novo ToDo"
+            placeholder="Adicionar novo todo"
             onChange={(e) => setNewTaskTitle(e.target.value)}
             value={newTaskTitle}
             autoFocus
